9458dead48cc11350f134b37a05cad5af169bbd2
Dorian <dorianoriginaldesings@gmail.com>
2023-12-12 12:12:12 +0100

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,68 @@
+import { QUESTIONS } from './questions/questions';
 
+const container = document.getElementById('letters-container');
+const letters = container.querySelectorAll('.letter');
+const buttons = document.getElementById('buttons');
+const questionTextElement = document.getElementById('question');
+const radius = 150; // Radio del círculo, ajusta según tus necesidades
+const totalLetters = letters.length;
+const angleIncrement = (2 * Math.PI) / totalLetters;
+
+const jumpedQuestions = [];
+let questionCounter = 0;
+let isDonutComplete = false;
+let currentLetter;
+
+letters.forEach((letter, index) => {
+	// Cambia el ángulo inicial para que la "A" esté arriba
+	const angle = (3 * Math.PI) / 2 + index * angleIncrement;
+	const x = radius * Math.cos(angle);
+	const y = radius * Math.sin(angle);
+
+	// Posiciona cada letra en el círculo
+	letter.style.left = `${x + radius}px`;
+	letter.style.top = `${y + radius}px`;
+});
+
+const printQuestion = questionToPrint => {
+	currentLetter = questionToPrint[questionCounter].id;
+	questionTextElement.textContent = questionToPrint[questionCounter].question;
+};
+
+const checkCounter = () => {
+	if (questionCounter >= QUESTIONS.length - 1) {
+		questionCounter = 0;
+		isDonutComplete = true;
+	} else {
+		questionCounter++;
+	}
+
+	if (isDonutComplete) {
+		printQuestion(jumpedQuestions);
+	} else {
+		printQuestion(QUESTIONS);
+	}
+};
+
+const checkAnswer = event => {
+	const type = event.target.dataset.type;
+	if (!type) return;
+	const letterElement = document.querySelector(
+		`[data-letter='${currentLetter}']`
+	);
+	letterElement.classList.remove('letter--jumped');
+	if (type === 'correct') {
+		letterElement.classList.add('letter--correct');
+	} else if (type === 'incorrect') {
+		letterElement.classList.add('letter--incorrect');
+	} else {
+		jumpedQuestions.push(QUESTIONS[questionCounter]);
+		letterElement.classList.add('letter--jumped');
+	}
+
+	checkCounter();
+};
+
+printQuestion(QUESTIONS);
+
+buttons.addEventListener('click', checkAnswer);
---
+++ src/js/questions/questions.js
@@ -0,0 +1,156 @@
+export const QUESTIONS = [
+	{
+		id: 'a',
+		question:
+			'con la A: Mecanismo para esparcir un líquido a presión, como el agua para el riego',
+		correctAnswer: 'aspersor'
+	},
+	{
+		id: 'b',
+		question: 'CON LA B: Apertura delantera de un pantalón',
+		correctAnswer: 'bragueta'
+	},
+	{
+		id: 'c',
+		question:
+			'CON LA C: Galería que rodea el patio principal de una iglesia o convento',
+		correctAnswer: 'claustro'
+	},
+	{
+		id: 'd',
+		question:
+			'CON LA D: Documento que expide un centro educativo o una corporación y que acredita un título o un grado académico',
+		correctAnswer: 'diploma'
+	},
+	{
+		id: 'e',
+		question:
+			'CON LA E: Nombre del rey visigodo que gobernó entre los años 687 y 702, y limitó la actividad económica de los judíos',
+		correctAnswer: 'égica'
+	},
+	{
+		id: 'f',
+		question: 'CON LA F: Brillo muy intenso',
+		correctAnswer: 'fulgor'
+	},
+	{
+		id: 'g',
+		question: 'CON LA G: País asiático con capital en Tifilis',
+		correctAnswer: 'georgia'
+	},
+	{
+		id: 'h',
+		question:
+			'CON LA H: MAsa de harina con manteca, muy sobada y que,al cocerse en el horno, forma muchas hojas delgadas superpuestas',
+		correctAnswer: 'hojaldre'
+	},
+	{
+		id: 'i',
+		question:
+			'CON LA I: Esperanza cuyo cumplimiento parece especialmente atractivo',
+		correctAnswer: 'ilusión'
+	},
+	{
+		id: 'j',
+		question:
+			'CON LA J: Soldado de infantería de la Guardia Imperial turca, reclutado a menudo entre hijos de cristianos',
+		correctAnswer: 'jenízaro'
+	},
+	{
+		id: 'k',
+		question:
+			'CON LA L: Daño o detrimento corporal causado por una herida, un golpe o una enfermedad ',
+		correctAnswer: 'lesión'
+	},
+	{
+		id: 'l',
+		question:
+			'CON LA L: Daño o detrimento corporal causado por una herida, un golpe o una enfermedad',
+		correctAnswer: 'lesión'
+	},
+	{
+		id: 'm',
+		question:
+			'CON LA M: Apellido del matemático que, a principios del siglo 20, introdujo el modelo de espacio-tiempo en cuatro dimensiones',
+		correctAnswer: 'minkowski'
+	},
+	{
+		id: 'n',
+		question: 'CON LA N: Insignificante, sin importancia',
+		correctAnswer: 'nimio'
+	},
+	{
+		id: 'ñ',
+		question:
+			'CONTIENE LA Ñ: En fútbol, acción en la que se sortea a un contrario pasándole el balón entre las piernas',
+		correctAnswer: 'caño'
+	},
+	{
+		id: 'o',
+		question: 'CON LA O: Cerro aislado que domina un llano',
+		correctAnswer: 'otero'
+	},
+	{
+		id: 'p',
+		question:
+			'CON LA P: Juego que consiste en tirar unas bolas y otra más pequeña, y ver quién se aproxima más a ésta con las grandes',
+		correctAnswer: 'petanca'
+	},
+	{
+		id: 'q',
+		question:
+			'CONTIENE LA Q: Se dice del triángulo que tiene todos sus lados iguales ',
+		correctAnswer: 'equilatero'
+	},
+	{
+		id: 'r',
+		question: 'CON LA R: Satisfacción a una pregunta, duda o dificultad',
+		correctAnswer: 'respuesta'
+	},
+	{
+		id: 's',
+		question:
+			'CON LA S: Antiguo instrumento musical de metal, consistente en un aro o herradura atravesado por varillas, que sonaba agitándolo ',
+		correctAnswer: 'sistro'
+	},
+	{
+		id: 't',
+		question:
+			'CON LA T: Cada uno de los pasos o gestiones que se establecen de manera oficial para la conclusión o resolución de un asunto ',
+		correctAnswer: 'trámite'
+	},
+	{
+		id: 'u',
+		question: 'CON LA U: Lugar en el que está situado algo',
+		correctAnswer: 'ubicación'
+	},
+	{
+		id: 'v',
+		question: 'CON LA V: Casa de recreo aislada en el campo',
+		correctAnswer: 'villa'
+	},
+	{
+		id: 'w',
+		question:
+			'CON LA W: Bebida alcohólica de alta graduación que se obtiene por destilación de cebada y otros cereales',
+		correctAnswer: 'ilusión'
+	},
+	{
+		id: 'x',
+		question:
+			'CONTIENE LA X: Enunciado o conjunto coherente de enunciados orales o escritos',
+		correctAnswer: 'texto'
+	},
+	{
+		id: 'y',
+		question:
+			'CONTIENE LA Y: Máquina o conjunto de máquinas que sirven para cambiar los decorados en el escenario de un teatro',
+		correctAnswer: 'tramoya'
+	},
+	{
+		id: 'z',
+		question:
+			'CON LA Z: Escarabajo que ataca los trigales, especialmente cuando los granos son tiernos ',
+		correctAnswer: 'zabro'
+	}
+];
